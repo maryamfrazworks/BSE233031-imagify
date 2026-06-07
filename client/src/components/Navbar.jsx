79970740a7525d7a12c00a4ae2930cf94a2dcea2
Maryam Fraz <maryamfrazworks@gmail.com>
2026-06-07 20:24:34 +0500

    made appcontext file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import {assets} from '../assets/assets'
 import { Link, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Navbar = () => {
-    const [user, setUser] = useState(true);
+    
+    const {user} =useContext(AppContext)
+
     const navigate = useNavigate()
 
   return (
@@ -13,18 +16,18 @@ const Navbar = () => {
     </Link>
     <div>
         {user ?
-        <div>
-            <button>
+        <div className='flex item-center gap-2 sm:gap-3'>
+            <button onClick={()=>navigate('/buy')} className='flex item-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
                 <img className='w-5' src={assets.credit_star} alt="" />
-                <p>Credits left : 50</p>
+                <p className='text-xs sm:text-sm font-medium text-gray-600'>Credits left : 50</p>
             </button>
-            <p>Hi, Maryam Fraz</p>
+            <p className='text-gray-600 max-sm:hidden pl-4'>Hi, Maryam Fraz</p>
             <div className='relative group'>
                 <img src={assets.profile_icon} className='w-10 drop-shadow' alt="" />
-            <div className='absoulte hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
-              <ul>
-                <li>Logout</li>
-              </ul>
+            <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+              <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
+              </ul >
              </div>
             </div>
         </div>
@@ -32,7 +35,7 @@ const Navbar = () => {
         <div className='flex item-center gap-2 sm:gap-5'>
             <p onClick={()=>navigate('/buy')} 
             className='cursor-pointer'>Pricing</p>
-            <button className=' bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
+            <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
         </div>
         }
     </div>
