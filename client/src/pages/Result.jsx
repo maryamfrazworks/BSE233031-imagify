1aac333cfaf6e79c1696bd329f0330b391933e0b
Maryam Fraz <maryamfrazworks@gmail.com>
2026-06-08 02:01:43 +0500

    wrote the code of buy credits

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -7,10 +7,9 @@ const Result = () => {
   const [isImageLoaded, setIsImageLoaded] = useState(false)
   const [loading, setloading] = useState(false)
   const [input, setInput] = useState('')
-
-  const onSubmiHandler = async (e)=> {
-
+  const onSubmitHandler = async (e) => {
   }
+  
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col min-h-[90vh] justify-center items-center'>
       <div>
@@ -22,7 +21,8 @@ const Result = () => {
     </div>
     {!isImageLoaded && 
     <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
-    <input onChange={e=>setInput(e.target.value)} value={input} type="text" placeholder='Describe what you want to generate' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color'/>
+    <input 
+    onChange={e => setInput(e.target.value)} value={input} type="text" placeholder='Describe what you want to generate' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color'/>
     <button type='submit' className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'>Generate</button>
     </div>
     }
